3b9944f257b35349833742e2fc1e456631c6c9cf
andrey-yalta <andrey.yalta.96qx@gmail.com>
2020-11-09 18:31:54 +0300

    create redux dev took

--- src/api/api.js
+++ src/api/api.js
@@ -2,15 +2,15 @@ import * as axios from "axios";
 
 
 const instance = axios.create({
-    baseURL:`http://localhost:3001/pizzas/?`
+    baseURL:`http://localhost:3001/pizzas/`
 
 });
 
 
 export const pizzaAPI  = {
     getPizzas (category=null, SelectedSortBy)   {
-
-        return instance.get(`${category !== null ? `category=${category}` : ''}&_sort=${SelectedSortBy.type}&_order=${
+        // debugger;
+        return instance.get(`${category !== null ? `?category=${category}` : '?'}&_sort=${SelectedSortBy.type}&_order=${
             SelectedSortBy.order
         } ` )
             .then(response=> {
